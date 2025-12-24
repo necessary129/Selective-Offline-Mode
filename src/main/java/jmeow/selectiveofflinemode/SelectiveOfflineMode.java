0f6cc66cd6b2e84b1d08f43501ef8a899b614d2a
noteness <noteness@riseup.net>
2025-12-24 10:26:03 +0530

    Update to Minecraft 1.21.11

--- src/main/java/jmeow/selectiveofflinemode/SelectiveOfflineMode.java
+++ src/main/java/jmeow/selectiveofflinemode/SelectiveOfflineMode.java
@@ -1,21 +1,21 @@
 package jmeow.selectiveofflinemode;
 
-import com.mojang.brigadier.arguments.IntegerArgumentType;
 import com.mojang.brigadier.arguments.StringArgumentType;
-import com.mojang.brigadier.builder.RequiredArgumentBuilder;
 import net.fabricmc.api.ModInitializer;
 
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.minecraft.command.permission.Permission;
+import net.minecraft.command.permission.PermissionLevel;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.text.Text;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import static com.mojang.brigadier.builder.LiteralArgumentBuilder.literal;
 import static net.minecraft.server.command.CommandManager.argument;
 
 public class SelectiveOfflineMode implements ModInitializer {
 	public static final String MOD_ID = "selective-offline-mode";
+    public static final Permission perm_admin = new Permission.Level(PermissionLevel.ADMINS);
 
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
@@ -31,7 +31,7 @@ public class SelectiveOfflineMode implements ModInitializer {
 		LOGGER.info("Selective Offline Mode starting");
 		CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment)
 				-> dispatcher.register(CommandManager.literal("allowplayer")
-				.requires(source -> source.hasPermissionLevel(2))
+				.requires(source -> source.getPermissions().hasPermission(perm_admin))
 				.then(argument("player", StringArgumentType.greedyString())
 				.executes(context -> {
 					final String playerName = StringArgumentType.getString(context, "player");
